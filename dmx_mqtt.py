f4421ec64644bcc8a5695ca70537dfc9c2823ad0
Carlos Fernandez Ruiz <carlos.fernandez.ruiz@estudiantat.upc.edu>
2025-05-01 12:29:08 +0200

    safe load

--- dmx_mqtt.py
+++ dmx_mqtt.py
@@ -9,7 +9,7 @@ import sys
 class DmxMqtt:
     def __init__(self, yamlpath:str):
         with open(yamlpath, 'r') as file:
-            conf:dict = yaml.safe_load(yamlpath)
+            conf:dict = yaml.safe_load(file)
 
         dmx_conf:dict = conf.get('dmx_conf')
         self.dmx_port:str = dmx_conf.get('dmx_port')
